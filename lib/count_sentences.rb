8e7741ed824a483959b0aaba80ed600c59add1b6
Abzed <ibraabzed@gmail.com>
2022-08-12 19:34:19 +0300

    commit tree 37669a57c6ab46e6a9e041ee2ccefdec8a79d304

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match(/\W$/) ? true : false
   end
 
   def question?
-
+    self.match(/\?/) ? true : false
   end
 
   def exclamation?
-
+    self.match(/\!/) ? true : false
   end
 
   def count_sentences
-
+    counts = self.match(/\./)
+    counts.count
   end
 end
